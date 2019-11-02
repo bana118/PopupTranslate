addcdf55125b7a618e724ad8e16add8ee61bbce1
bana <bana.titech@gmail.com>
2019-11-02 22:26:49 +0900

    wait 500ms for translate not to block, and support the text including ? and !

--- js/background.js
+++ js/background.js
@@ -1,13 +1,44 @@
-chrome.contextMenus.create({id:"translate",title:"translate:%s",contexts:["selection"]});
+function sleep(num) {
+    return new Promise(r => setTimeout(r, num));
+}
+function translate(glottologist, text) {
+    return glottologist.t(text, "ja")
+}
+async function asyncTranslate(glottologist, textArray) {
+    var result = "";
+    for (const t of textArray) {
+        await sleep(500);
+        const translatedText = await translate(glottologist, t);
+        console.log(translatedText);
+        result += translatedText;
+    }
+    return result;
+}
 
-chrome.contextMenus.onClicked.addListener(function (info,tab){
-    if(info.menuItemId == "translate") {
+chrome.contextMenus.create({ id: "translate", title: "translate:%s", contexts: ["selection"] });
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId == "translate") {
         const glot = new Glottologist();
-        const splitSelectionText = info.selectionText.split(".");
-        for(i=0;i<splitSelectionText.length-1;i++) {
-            splitSelectionText[i] += ".";
-        }
-        const promiseArray = splitSelectionText.map(text=>
+        const splitSelectionText = info.selectionText.split(/(?<=[\.\!\?])/g);
+        asyncTranslate(glot, splitSelectionText).then(translated => {
+            console.log(translated);
+            var queryInfo = {
+                active: true,
+                windowId: chrome.windows.WINDOW_ID_CURRENT
+            };
+            chrome.tabs.query(queryInfo, function (result) {
+                var currentTab = result.shift();
+                var message = {
+                    command: "translate",
+                    text: translated
+                };
+                chrome.tabs.sendMessage(currentTab.id, message, function (response) {
+                    console.log(response);
+                });
+            });
+        });
+        /*const promiseArray = splitSelectionText.map(text=>
             glot.t(text, "ja")
         );
         Promise.all(promiseArray).then(function(translatedArray) {
@@ -27,24 +58,7 @@ chrome.contextMenus.onClicked.addListener(function (info,tab){
                     console.log(response);
                 });
             });
-        })
-        /*glot.t(info.selectionText, "ja").then(translated => {
-            console.log(translated);
-            var queryInfo = {
-                active: true,
-                windowId: chrome.windows.WINDOW_ID_CURRENT
-            };
-            chrome.tabs.query(queryInfo, function (result) {
-                var currentTab = result.shift();
-                var message = {
-                    command: "translate",
-                    text: translated
-                };
-                chrome.tabs.sendMessage(currentTab.id, message, function(response){
-                    console.log(response);
-                });
-            });
-        });*/
+        })*/
     }
 });
 
