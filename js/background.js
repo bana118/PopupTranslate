e069bd5e4f280258d8e6587a2d200c56187bb2e7
bana <bana.titech@gmail.com>
2019-11-02 19:35:51 +0900

    implement multiple texts translation

--- js/background.js
+++ js/background.js
@@ -3,7 +3,15 @@ chrome.contextMenus.create({id:"translate",title:"translate:%s",contexts:["selec
 chrome.contextMenus.onClicked.addListener(function (info,tab){
     if(info.menuItemId == "translate") {
         const glot = new Glottologist();
-        glot.t(info.selectionText, "ja").then(translated => {
+        const splitSelectionText = info.selectionText.split(".");
+        for(i=0;i<splitSelectionText.length-1;i++) {
+            splitSelectionText[i] += ".";
+        }
+        const promiseArray = splitSelectionText.map(text=>
+            glot.t(text, "ja")
+        );
+        Promise.all(promiseArray).then(function(translatedArray) {
+            const translated = translatedArray.join("");
             console.log(translated);
             var queryInfo = {
                 active: true,
@@ -19,7 +27,24 @@ chrome.contextMenus.onClicked.addListener(function (info,tab){
                     console.log(response);
                 });
             });
-        });
+        })
+        /*glot.t(info.selectionText, "ja").then(translated => {
+            console.log(translated);
+            var queryInfo = {
+                active: true,
+                windowId: chrome.windows.WINDOW_ID_CURRENT
+            };
+            chrome.tabs.query(queryInfo, function (result) {
+                var currentTab = result.shift();
+                var message = {
+                    command: "translate",
+                    text: translated
+                };
+                chrome.tabs.sendMessage(currentTab.id, message, function(response){
+                    console.log(response);
+                });
+            });
+        });*/
     }
 });
 
